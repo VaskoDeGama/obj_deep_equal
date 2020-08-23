c6e8632eec5d9f09adfc1949320bec0bbd461f03
VaskoDeGama <vasr161095@gmail.com>
2020-08-23 18:52:33 +0800

    added some tests

--- src/objDeepEqual.spec.js
+++ src/objDeepEqual.spec.js
@@ -1,7 +1,30 @@
 const objDeepEqual = require('./objDeepEqual.js')
 
+expect.extend({
+  toBeBoolean(received) {
+    return typeof received === 'boolean'
+      ? {
+          message: () => `expected ${received} to be boolean`,
+          pass: true,
+        }
+      : {
+          message: () => `expected ${received} to be boolean`,
+          pass: false,
+        }
+  },
+})
+
 describe('DeepEqual:', () => {
-  test('well be', () => {
+  test('should be defined', () => {
     expect(objDeepEqual).toBeDefined()
+  })
+  test('should return boolean', () => {
+    const result = objDeepEqual({}, {})
+    expect(result).toBeBoolean()
+  })
+  test('should be true if compare same object', () => {
+    const obj = {}
+    const result = objDeepEqual(obj, obj)
+    expect(result).toBeTruthy()
   })
 })
